afed2ce7648e6ab395b741aacecb265fccd53f89
physcoccode <pranjaltech10@gmail.com>
2020-05-18 17:25:47 +0530

    style: fix style

--- src/Service/Filesystem.php
+++ src/Service/Filesystem.php
@@ -10,10 +10,13 @@ namespace Scrawler\Service;
 
 use Scrawler\Scrawler;
 
- Class Filesystem extends League\Flysystem\Filesystem{
-      public function saveRequest($path=''){
-          $files= Scrawler::engine()->request()->files->all();
-          foreach($files as $file){
-            $filesystem->write($path, $file);          }
-      }
- }
+class Filesystem extends League\Flysystem\Filesystem
+{
+    public function saveRequest($path='')
+    {
+        $files= Scrawler::engine()->request()->files->all();
+        foreach ($files as $file) {
+            $filesystem->write($path, $file);
+        }
+    }
+}
