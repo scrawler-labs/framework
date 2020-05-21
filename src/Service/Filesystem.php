f0eed72d04ab2e637e3a0793524b7bf343cc5401
physcoccode <pranjaltech10@gmail.com>
2020-05-21 14:11:00 +0530

    docs: docblock filesystem

--- src/Service/Filesystem.php
+++ src/Service/Filesystem.php
@@ -10,20 +10,25 @@ namespace Scrawler\Service;
 
 use Scrawler\Scrawler;
 
- Class Filesystem extends \League\Flysystem\Filesystem{
+class Filesystem extends \League\Flysystem\Filesystem
+{
+
       /**
-       * Upload all available file from request
+       * Stores the files in request to  specific path
+       *
+       * @param string $path
+       * @return array
        */
-      public function saveRequest($path=''){
-          $uploaded = [];
-          $files= Scrawler::engine()->request()->files->all();
-          foreach($files as $name => $file){
+    public function saveRequest(String $path='')
+    {
+        $uploaded = [];
+        $files= Scrawler::engine()->request()->files->all();
+        foreach ($files as $name => $file) {
             $content = file_get_contents($file->getPathname());
             $filename=md5($file->getClientOriginalName()).uniqid().'.'.$file->getClientOriginalExtension();
-            $this->write($path.$filename, $content);  
-            $uploaded[$name] =  url('/storage/'.$filename);     
-          }
-          return $uploaded;
-      }
-
- }
+            $this->write($path.$filename, $content);
+            $uploaded[$name] =  url('/storage/'.$filename);
+        }
+        return $uploaded;
+    }
+}
