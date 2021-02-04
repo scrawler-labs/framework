ea6101befc37e62660766a131151ce2c7a96389d
physcocode <pranjaltech10@gmail.com>
2021-02-05 01:24:47 +0530

    feat: response class

---
+++ src/Service/Http/Response.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Scrawler Request Object
+ *
+ * @package: Scrawler
+ * @author: Pranjal Pandey
+ */
+
+namespace Scrawler\Service\Http;
+
+use Scrawler\Scrawler;
+
+class Request extends \Symfony\Component\HttpFoundation\Response
+{
+
+}
